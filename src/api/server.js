8d2c6c055c74d82c1a2083a352bdda4f9845bf72
fhdyt <fhidayat2206@gmail.com>
2020-08-15 18:33:00 +0700

    adfadfad

--- src/api/server.js
+++ src/api/server.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export default axios.create({
  baseURL: 'https://guarded-plains-47822.herokuapp.com'
   //baseURL: 'https://cors-anywhere.herokuapp.com/https://guarded-plains-47822.herokuapp.com',
- //baseURL: 'http://b9c995aac1d7.ngrok.io'
+ //baseURL: 'http://997b30ca5e43.ngrok.io'
   
 });
   
